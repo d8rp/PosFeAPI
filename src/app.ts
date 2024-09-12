19e5c591ec9003fb600c86eb33df48b1e920fa3e
d8rp <thanh062007gmail.com>
2024-09-12 14:29:54 +0700

    fix: add body parsers

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,8 @@ import ConnectToDB from "./db/Connect";
     const app = express()
     const port = 3000
 
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
     app.use(orderRoutes)
 
     app.listen(port, () => console.log(`Server started on port ${port}`))
